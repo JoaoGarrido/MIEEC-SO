fda4592e4b910d3c4b94438ec1e56f34a5c9ed8e
Taillow <my.blog.jp@gmail.com>
2019-03-14 11:00:10 +0000

    P4 Ex1_a/b

---
+++ P4/Ex1_a.c
@@ -0,0 +1,51 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int a = 0;
+int b = 0;
+
+void *sum(void *arg){
+    int res=a+b;
+    printf("Sum: %d\n", res);
+}
+
+void *sub(void *arg){
+    int res=a-b;
+    printf("Sub: %d\n", res);
+}
+
+void *mult(void *arg){
+    int res=a*b;
+    printf("Mult: %d\n", res);
+}
+
+void *divi(void *arg){
+    int res=a/b;
+    printf("Div: %d\n", res);
+}
+
+int main(int argc, char *argv[]){
+    if(argc != 3) return -1;
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    pthread_attr_t attr[4];
+    pthread_t id[4];
+    /*for(int i = 0; i < 4; i++){
+        pthread_attr_init(&(attr[i]));
+    }*/
+    pthread_create(&(id[0]), NULL, sum, NULL);
+    pthread_create(&(id[1]), NULL, sub, NULL);
+    pthread_create(&(id[2]), NULL, mult, NULL);
+    pthread_create(&(id[3]), NULL, divi, NULL);
+    
+    pthread_join((id[0]), NULL);
+    pthread_join((id[1]), NULL);
+    pthread_join((id[2]), NULL);
+    pthread_join((id[3]), NULL);
+}
---
+++ P4/Ex1_b.c
@@ -0,0 +1,64 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef struct _input_struct
+{
+    int a, b;
+}input_struct;
+
+void *sum(void *arg){
+    int res = *(int*)arg+*(int *)(arg+sizeof(int));
+    printf("Sum: %d\n", res);
+}
+
+void *sub(void *arg){
+    int res = *(int*)arg-*(int *)(arg+sizeof(int));
+    printf("Sub: %d\n", res);
+}
+
+void *mult(void *arg){
+    input_struct *values = (input_struct *)arg;
+    int res = values->a*values->b;
+    printf("Mult: %d\n", res);
+}
+
+void *divi(void *arg){
+    input_struct *values = (input_struct *)arg;
+    int res = values->a/values->b;
+    printf("Div: %d\n", res);
+}
+
+int main(int argc, char *argv[]){
+    //Reading input
+    if(argc != 3) return -1;
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    //Array
+    int args[2];
+    args[0]=a;
+    args[1]=b;
+    //Struct
+    input_struct input;
+    input.a = a;
+    input.b = b;
+    pthread_attr_t attr[4];
+    pthread_t id[4];
+
+    /*for(int i = 0; i < 4; i++){
+        pthread_attr_init(&(attr[i]));
+    }*/
+    pthread_create(&(id[0]), NULL, sum, (void *) args);
+    pthread_create(&(id[1]), NULL, sub, (void *) args);
+    pthread_create(&(id[2]), NULL, mult, (void *) &input);
+    pthread_create(&(id[3]), NULL, divi, (void *) &input);
+    pthread_join((id[0]), NULL);
+    pthread_join((id[1]), NULL);
+    pthread_join((id[2]), NULL);
+    pthread_join((id[3]), NULL);
+}
